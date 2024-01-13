7e05fa389e7c30af5d97b9ab5aa2d6c64c812e37
norbastic <norbert.faragooo@gmail.com>
2024-01-13 22:21:50 +0100

    Restructuring project

--- Models/DeviceDiscoveryResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace AC.Library.Models;
-
-internal class DeviceDiscoveryResponse
-{
-    public string? Json { get; set; }
-
-    public string? Address { get; set; }
-}
--- Models/ScannedDevice.cs
+++
@@ -1,9 +0,0 @@
-namespace AC.Library.Models;
-
-public class ScannedDevice
-{
-    public string? Id { get; set; }
-    public string? Name { get; set; }
-    public string? Address { get; set; }
-    public string? Type { get; set; }
-}
